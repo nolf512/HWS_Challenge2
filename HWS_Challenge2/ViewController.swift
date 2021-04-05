25dccbf8c2dcbe24994fc6a36a0fcdcc1160d4e1
nofprog <k.jin3217@gmail.com>
2021-04-05 23:45:38 +0900

    tableView

--- HWS_Challenge2/ViewController.swift
+++ HWS_Challenge2/ViewController.swift
@@ -7,12 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("did select")
+    }
+    
+    
 
 
 }
